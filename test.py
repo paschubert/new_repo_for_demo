68df28120d8416bf307008c3373403ccd56c1ec8
Peter Schubert <Peter.Schubert@hhu.de>
2021-07-21 18:08:20 +0200

    Modification on local machine

--- test.py
+++ test.py
@@ -6,6 +6,8 @@ import tellurium as te # Python-based modeling environment for kinetic models
 
 print("Hello World.")
 
+print("modification done on local machine")
+
 r = te.loada("species a=3")
 r.simulate()
 r.plot()
